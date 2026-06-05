f11a7ac1ac60f05695dd0ba04498bd990057cc6d
hughie2026 <utmhughie@gmail.com>
2026-06-05 10:47:23 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
+const navToggle = document.querySelector(".nav-toggle");
+const siteNav = document.querySelector(".site-nav");
 
+if (navToggle && siteNav) {
+  navToggle.addEventListener("click", () => {
+    siteNav.classList.toggle("open");
+  });
+}
+
+const currentPath = window.location.pathname.split("/").pop() || "index.html";
+const navLinks = document.querySelectorAll(".site-nav a");
+
+navLinks.forEach((link) => {
+  const href = link.getAttribute("href");
+
+  if (href === currentPath) {
+    link.classList.add("active");
+  }
+
+  link.addEventListener("click", () => {
+    siteNav.classList.remove("open");
+  });
+});
